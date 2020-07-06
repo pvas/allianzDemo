75e03e8fbba9b10b5f2fad4ef3165cf104131424
Ivan Silva <isilva@heinsohn.com.co>
2020-07-06 18:31:14 +0200

    * ► unit test in spring boot

--- src/test/java/com/allianz/coreader/controller/COReaderControllerTest.java
+++ src/test/java/com/allianz/coreader/controller/COReaderControllerTest.java
@@ -2,10 +2,12 @@ package com.allianz.coreader.controller;
 
 import static org.junit.Assert.assertNotNull;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.allianz.coreader.config.AbstractTest;
  
-public class COReaderControllerTest {
+public class COReaderControllerTest extends AbstractTest {
 
 	@Autowired
 	COReaderController controller;
