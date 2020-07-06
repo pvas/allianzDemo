e7d5ee22cf267c5436eedac25f73877d2cc6c4bf
Ivan Silva <isilva@heinsohn.com.co>
2020-07-06 13:26:56 +0200

    * ► Spring boot initializer code

---
+++ src/main/java/com/allianz/coreader/CoreaderApplication.java
@@ -0,0 +1,13 @@
+package com.allianz.coreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreaderApplication.class, args);
+	}
+
+}
