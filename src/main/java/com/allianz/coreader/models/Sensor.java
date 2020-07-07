0af9a4834c1370de09d008d04ea6dbe27b6ef5ad
pvas <pv.as2007@gmail.com>
2020-07-07 23:13:41 +0200

    Implementation of City repository and fix generic imports

--- src/main/java/com/allianz/coreader/models/Sensor.java
+++ src/main/java/com/allianz/coreader/models/Sensor.java
@@ -4,7 +4,11 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
 
 @Entity
 @Getter
