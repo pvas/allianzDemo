9b615c738d5004878c6004ecc548bf0b4e3b9c23
pvas <pv.as2007@gmail.com>
2020-07-10 09:55:41 +0200

    Complete implementation of Swagger, without UI.

--- src/main/java/com/allianz/coreader/CoreaderApplication.java
+++ src/main/java/com/allianz/coreader/CoreaderApplication.java
@@ -4,14 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.regex;
+
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 
-
-
-@SpringBootApplication
 @ComponentScan("com.allianz.coreader")
+@SpringBootApplication
+@EnableSwagger2
 public class CoreaderApplication {
 
 	public static void main(String[] args) {
@@ -24,4 +31,19 @@ public class CoreaderApplication {
 		return factory.getValidator();
 	}
 
+	@Bean
+	public Docket apiDocket() {
+
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(new ApiInfoBuilder()
+						.description("RESTful server application which provides this backend service to store CO2 emission from sensor")
+						.title("Allianz CO2 Reader")
+						.version("1.0.0")
+						.build())
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.allianz.coreader.controllers"))
+				.paths(PathSelectors.any())
+				.paths(regex("(/api/v1.*)|(/register.*)"))
+				.build();
+	}
 }
